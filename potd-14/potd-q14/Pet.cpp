5e68b275c86f91982eb69bc16a1606be02515076
HaoranTang <46015479+HaoranTang@users.noreply.github.com>
2019-06-04 16:12:32 +0800

    Add files via upload

---
+++ potd-14/potd-q14/Pet.cpp
@@ -0,0 +1,34 @@
+// Pet.cpp
+#include "Pet.h"
+#include "Animal.h"
+#include <string>
+#include <iostream>
+using namespace std;
+string Pet::getName(){
+    return name_;
+}
+
+void Pet::setName(string name){
+    name_ = name;
+}
+
+string Pet::getOwnerName(){
+    return owner_name_;
+}
+
+void Pet::setOwnerName(string owner_name){
+    owner_name_ = owner_name;
+}
+
+string Pet::print(){
+    return "My name is "+name_;
+}
+
+Pet::Pet():Animal("cat","fish"){
+    name_ = "Fluffy";
+    owner_name_ = "Cinda";
+}
+Pet::Pet(string type, string food, string name, string owner_name):Animal(type,food){
+    name_ = name;
+    owner_name_ = owner_name;
+}
---
+++ potd-14/potd-q14/Pet.h
@@ -0,0 +1,18 @@
+// Pet.h
+#pragma once
+#include <string>
+#include "Animal.h"
+using namespace std;
+class Pet : public Animal {
+public:
+    string getName();
+    void setName(string name);
+    string getOwnerName();
+    void setOwnerName(string owner_name);
+    string print();
+    Pet();
+    Pet(string type, string food, string name, string owner_name);
+private:
+    string name_;
+    string owner_name_;
+};
